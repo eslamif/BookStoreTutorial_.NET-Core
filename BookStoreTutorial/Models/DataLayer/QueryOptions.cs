c7d095fcf464a439575230f7bcb9db3b2dd4c7b8
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-10-29 14:14:03 -0700

    WhereOptions and WhereClauses created.

---
+++ BookStoreTutorial/Models/DataLayer/QueryOptions.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.DataLayer
+{
+    public class QueryOptions<T>
+    {
+        public Expression<Func<T, Object>> OrderBy { get; set; }
+        public string OrderByDirection { get; set; } = "asc";
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+
+        public string Includes
+        {
+            set => includes = value.Replace(" ", "").Split(",");
+        }
+
+        public WhereClauses<T> WhereClauses { get; set; }
+
+        public Expression<Func<T, bool>> Where
+        {
+            set
+            {
+                if (WhereClauses == null)
+                {
+                    WhereClauses = new WhereClauses<T>();
+                }
+                WhereClauses.Add(value);
+            }
+        }
+
+        public bool HasWhere => WhereClauses != null;
+        public bool HasOrderBy => OrderBy != null;
+        public bool HasPaging => PageNumber > 0 && PageSize > 0;
+
+        private string[] includes;
+
+        public string[] GetIncludes => includes ?? new string[0];
+    }
+}
---
+++ BookStoreTutorial/Models/DataLayer/WhereClauses.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.DataLayer
+{
+    public class WhereClauses<T> : List<Expression<Func<T, bool>>> { }
+}
