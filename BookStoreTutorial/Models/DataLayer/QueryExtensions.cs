b22bb7b5bc7721d43e15dea234d43d443cee2c9f
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-10-29 13:28:22 -0700

    PageBy() extension created.

---
+++ BookStoreTutorial/Models/DataLayer/QueryExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.DataLayer
+{
+    public static class QueryExtensions
+    {
+        public static IQueryable<T> PageBy<T>(this IQueryable<T> items, int pageNumber, int pageSize)
+        {
+            //Skip pages and return remaining up to pageSize
+            return items
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize);
+        }
+    }
+}
