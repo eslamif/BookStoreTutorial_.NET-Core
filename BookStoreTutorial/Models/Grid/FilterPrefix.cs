48e632a2167bf313fc4048a89e33ff4bd9467495
Frank Eslami <eslamifrank@gmail.com>
2020-10-31 10:17:09 -0700

    BookStoreTutorial created.

---
+++ BookStoreTutorial/Models/Grid/FilterPrefix.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.Grid {
+    public static class FilterPrefix {
+        public const string Genre = "genre-";
+        public const string Price = "price-";
+        public const string Author = "author-";
+    }
+}
