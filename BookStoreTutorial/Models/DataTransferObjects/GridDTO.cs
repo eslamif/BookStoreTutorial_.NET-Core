d71f1adeafd4fa0f21daf2c2eb629f8d6d90b342
Frank Eslami <eslamifrank@gmail.com>
2020-10-31 07:44:23 -0700

    Initial data transfer objects created.

---
+++ BookStoreTutorial/Models/DataTransferObjects/GridDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.DataTransferObjects {
+    public class GridDTO {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
+        public string SortField { get; set; }
+        public string SortDirection { get; set; } = "asc";
+    }
+}
