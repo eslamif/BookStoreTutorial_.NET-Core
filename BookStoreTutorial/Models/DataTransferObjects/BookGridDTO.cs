cbffaceb48467f1d39130eece57e2008aeef9ae7
Frank Eslami <eslamifrank@gmail.com>
2020-11-01 11:33:10 -0800

    BookGridDTO created.

---
+++ BookStoreTutorial/Models/DataTransferObjects/BookGridDTO.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.DataTransferObjects {
+    public class BookGridDTO : GridDTO {
+        [JsonIgnore]
+        public const string DefaultFilter = "all";
+
+        public string Author { get; set; } = DefaultFilter;
+        public string Genre { get; set; } = DefaultFilter;
+        public string Price { get; set; } = DefaultFilter;
+    }
+}
