6f8160f378fd829487ca42aa3bf64e6235e84544
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-11-04 07:25:44 -0800

    CookieExtensionMethods created.

---
+++ BookStoreTutorial/Models/ExtensionMethods/CookieExtensionMethods.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Models.ExtensionMethods
+{
+    public static class CookieExtensionMethods
+    {
+        public static string GetString(this IRequestCookieCollection cookies, string key) => cookies[key];
+
+        public static int? GetInt32(this IRequestCookieCollection cookies, string key) =>
+            int.TryParse(cookies[key], out int i) ? i : (int?)null;
+
+        public static T GetObject<T>(this IRequestCookieCollection cookies, string key)
+        {
+            var value = cookies[key];
+            return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
+        }
+    }
+}
