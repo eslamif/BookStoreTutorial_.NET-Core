313e348203cac40e42785d34fcfbdcb609b83ad6
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-11-04 10:14:09 -0800

    Book list controller works.

--- BookStoreTutorial/Models/DataTransferObjects/GridDTO.cs
+++ BookStoreTutorial/Models/DataTransferObjects/GridDTO.cs
@@ -5,8 +5,8 @@ using System.Threading.Tasks;
 
 namespace BookStoreTutorial.Models.DataTransferObjects {
     public class GridDTO {
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 4;
         public string SortField { get; set; }
         public string SortDirection { get; set; } = "asc";
     }
