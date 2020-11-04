668e817b94bf3cde73c975d3b21c9b0a76ce19a4
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-11-04 07:32:52 -0800

    Added setters to CookieExtensionMethods.

--- BookStoreTutorial/Models/ExtensionMethods/CookieExtensionMethods.cs
+++ BookStoreTutorial/Models/ExtensionMethods/CookieExtensionMethods.cs
@@ -19,5 +19,26 @@ namespace BookStoreTutorial.Models.ExtensionMethods
             var value = cookies[key];
             return value == null ? default(T) : JsonConvert.DeserializeObject<T>(value);
         }
+
+        public static void SetString(this IResponseCookies cookies, string key, string value, int days = 30)
+        {
+            cookies.Delete(key);
+            
+            if (days == 0)
+            {
+                cookies.Append(key, value);
+            }
+            else
+            {
+                CookieOptions options = new CookieOptions { Expires = DateTime.Now.AddDays(days) };
+                cookies.Append(key, value, options);
+            }
+        }
+
+        public static void SetInt32(this IResponseCookies cookies, string key, int value, int days = 30) =>
+            cookies.SetString(key, value.ToString(), days);
+
+        public static void SetObject<T>(this IResponseCookies cookies, string key, T value, int days = 30) =>
+            cookies.SetString(key, JsonConvert.SerializeObject(value), days);
     }
 }
