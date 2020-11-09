1b7715720dfb1b64538d1f0820f3b3fceb48a03c
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-11-09 07:30:38 -0800

    Operation created.

---
+++ BookStoreTutorial/Areas/Admin/Models/Operation.cs
@@ -0,0 +1,15 @@
+﻿using BookStoreTutorial.Models.ExtensionMethods;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Areas.Admin.Models
+{
+    public static class Operation
+    {
+        public static bool IsAdd(string action) => action.EqualNoCase("add");
+
+        public static bool IsDelete(string action) => action.EqualNoCase("delete");
+    }
+}
