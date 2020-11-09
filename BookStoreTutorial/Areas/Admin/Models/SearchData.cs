6eddbadb927deece741ae1a6b1d3ad6f9941454e
Frank Eslami <frank.eslami@mbc.ca.gov>
2020-11-09 07:39:01 -0800

    SearchData class created.

---
+++ BookStoreTutorial/Areas/Admin/Models/SearchData.cs
@@ -0,0 +1,43 @@
+﻿using BookStoreTutorial.Models.ExtensionMethods;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Areas.Admin.Models
+{
+    public class SearchData
+    {
+        private const string SearchKey = "search";
+        private const string TypeKey = "searchtype";
+
+        private ITempDataDictionary tempData { get; set; }
+
+        public string SearchTerm
+        {
+            get => tempData.Peek(SearchKey)?.ToString();
+            set => tempData[SearchKey] = value;
+        }
+
+        public string Type
+        {
+            get => tempData.Peek(TypeKey)?.ToString();
+            set => tempData[TypeKey] = value;
+        }
+
+        public bool HasSearchTerm => !string.IsNullOrEmpty(SearchTerm);
+        public bool IsBook => Type.EqualNoCase("book");
+        public bool IsAuthor => Type.EqualNoCase("author");
+        public bool IsGenre => Type.EqualNoCase("genre");
+
+        public SearchData(ITempDataDictionary tempData) => this.tempData = tempData;
+
+        public void Clear()
+        {
+            tempData.Remove(SearchKey);
+            tempData.Remove(TypeKey);
+        }
+    }
+}
