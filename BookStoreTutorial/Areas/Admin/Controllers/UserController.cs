f9dedb0f87c21de8f9a20e7d6ca46888c521fa9e
Frank Eslami <eslamifrank@gmail.com>
2020-11-11 08:06:16 -0800

    UserController created.

---
+++ BookStoreTutorial/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStoreTutorial.Areas.Admin.Models;
+using BookStoreTutorial.Models.DomainModels;
+using BookStoreTutorial.Models.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookStoreTutorial.Areas.Admin.Controllers
+{
+    public class UserController : Controller
+    {
+        private UserManager<User> userManager;
+        private RoleManager<IdentityRole> roleManager;
+
+        public UserController(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            List<User> users = new List<User>();
+
+            foreach (User user in userManager.Users)
+            {
+                user.RoleNames = await userManager.GetRolesAsync(user);
+                users.Add(user);
+            }
+
+            UserViewModel model = new UserViewModel
+            {
+                Users = users,
+                Roles = roleManager.Roles
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string id)
+        {
+            User user = await userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+
+                if (!result.Succeeded)
+                {
+                    string errorMessage = "";
+
+                    foreach (IdentityError error in result.Errors)
+                    {
+                        errorMessage += error.Description + " | ";
+                    }
+
+                    TempData["message"] = errorMessage;
+                }
+            }
+
+            return RedirectToAction("Index", "User");
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(RegisterViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                User user = new User { UserName = model.Username };
+                var result = await userManager.CreateAsync(user, model.Password);
+
+                if (result.Succeeded)
+                {
+                    RedirectToAction("Index");
+                }
+                else
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError("", error.Description);
+                    }
+                }
+            }
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddToAdmin(string id)
+        {
+            IdentityRole adminRole = await roleManager.FindByNameAsync("Admin");
+
+            if (adminRole == null)
+            {
+                TempData["message"] = "Admin role does not exist. Click 'Create Admin Role' to create it.";
+            }
+            else
+            {
+                User user = await userManager.FindByIdAsync(id);
+                await userManager.AddToRoleAsync(user, adminRole.Name);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> RemoveFromAdmin(string id)
+        {
+            User user = await userManager.FindByIdAsync(id);
+            var result = await userManager.RemoveFromRoleAsync(user, "Admin");
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAdminRole()
+        {
+            var result = await roleManager.CreateAsync(new IdentityRole("Admin"));
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> DeleteRole(string id)
+        {
+            IdentityRole role = await roleManager.FindByIdAsync(id);
+            var result = await roleManager.DeleteAsync(role);
+
+            if (!result.Succeeded)
+            {
+                foreach (var error in result.Errors)
+                {
+                    ModelState.AddModelError("", error.Description);
+                }
+            }
+
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ BookStoreTutorial/Areas/Admin/Models/UserViewModel.cs
@@ -0,0 +1,13 @@
+﻿using BookStoreTutorial.Models.DomainModels;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStoreTutorial.Areas.Admin.Models {
+    public class UserViewModel {
+        public IEnumerable<User> Users { get; set; }
+        public IEnumerable<IdentityRole> Roles { get; set; }
+    }
+}
